2c6116633cfa4abc5017f6f2788ac65b264ef8c8
sajaalgadhi <saja.algadhi@outlook.com>
2019-02-10 21:43:24 +0300

    Css looks horrible

--- weather.js
+++ weather.js
@@ -7,16 +7,15 @@ axios({
 })
   .then((response) => {
       console.log(response);
-      // eslint-disable-next-line prefer-destructuring
+      
       const temp = response.data.main.temp;
-      // eslint-disable-next-line no-undef
+      
       $('body').append(`<h4>${temp}</h4>`);
-      // eslint-disable-next-line prefer-destructuring
-      // const icon = response.data.weather[0].icon;
-      // // eslint-disable-next-line no-undef
-      // $('body').append(`<h4>${icon}</h4>`);
+      
+      let icon = 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png'
+       $('img').attr('src', icon)
       const weathr = response.data.weather[0].description;
-    // eslint-disable-next-line no-undef
+    
       $('body').append(`<h5>${weathr}</h5>`);
       
     
